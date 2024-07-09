94712d5d8853f208a3900a8870900d3a10ce05ae
chingu-bingu <leonbarber1304@gmail.com>
2024-07-09 21:42:31 +1000

    Update log.js

--- log.js
+++ log.js
@@ -1,1 +1,17 @@
+// log.js
+exports.handler = async function(event, context) {
+  const ip = event.headers['x-forwarded-for'] || event.headers['client-ip'];
+  const browser = event.headers['user-agent'];
+  const timestamp = new Date().toISOString();
+
+  const log = `IP: ${ip} - Browser: ${browser} - Time: ${timestamp}\n`;
+  console.log(log);
+
+  return {
+    statusCode: 302,
+    headers: {
+      Location: 'https://www.example.com', // URL to redirect to
+    },
+  };
+};
 
